52bbdd93a4ec95a3ba5e33f1cb9b82441c013417
MarcinFed <fedorowicz.marcin02@gmail.com>
2024-07-27 20:47:32 +0200

    Creating general look, in progress

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,58 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Groceries',
+      amount: 50.98,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Train ticket',
+      amount: 20.0,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'Movie ticket',
+      amount: 10.0,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Laptop',
+      amount: 1000.0,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expenses'),
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/expenses_list/expense_item.dart
@@ -0,0 +1,45 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({required this.expense, super.key});
+
+  final Expense expense;
+
+  @override
+  Widget build(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 20,
+      ),
+      child: Card(
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(
+                      expense.icon,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      expense.formattedDate,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
