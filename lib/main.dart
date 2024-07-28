8c1615eb36ceebe64b61ea10526b1d6bc2018eb5
MarcinFed <fedorowicz.marcin02@gmail.com>
2024-07-28 21:16:02 +0200

    Changing the theme parameters

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(
+    255, 163, 134, 214));
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.all(16)),
+      ),
       title: 'Expenses Tracker',
-      home: Expenses(),
+      home: const Expenses(),
     ),
   );
 }
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -10,34 +10,37 @@ class ExpenseItem extends StatelessWidget {
   Widget build(context) {
     return Padding(
       padding: const EdgeInsets.symmetric(
-        vertical: 16,
+        vertical: 8,
         horizontal: 20,
       ),
       child: Card(
-        child: Column(
-          children: [
-            Text(expense.title),
-            const SizedBox(height: 4),
-            Row(
-              children: [
-                Text('\$${expense.amount.toStringAsFixed(2)}'),
-                const Spacer(),
-                Row(
-                  children: [
-                    Icon(
-                      expense.icon,
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      expense.formattedDate,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Text(expense.title),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  Text('\$${expense.amount.toStringAsFixed(2)}'),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      Icon(
+                        expense.icon,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        expense.formattedDate,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
